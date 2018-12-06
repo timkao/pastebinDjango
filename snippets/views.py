df425c2efa7aa46ee91397f11a63a069f3d1b5b2
Tim Kao <pakuya.kao@gmail.com>
2018-12-05 21:32:13 -0500

    use modelSerializer and basic view settings

--- snippets/views.py
+++ snippets/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from .models import Snippet
+from .serializers import SnippetSerializer
+
+@csrf_exempt
+def snippet_list(request):
+  if request.method == 'GET':
+    snippets = Snippet.objects.all()
+    serializer = SnippetSerializer(snippets, many=True)
+    return JsonResponse(serializer.data, safe=False)
+  elif request.method == 'POST':
+    data = JSONParser().parse(request)
+    serializer = SnippetSerializer(data=data)
+    if serializer.is_valid():
+      serializer.save()
+      return JsonResponse(serializer.data, status=201)
+    return JsonResponse(serializer.errors, status=400)
