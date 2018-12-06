cf13da878e5cfcc232e553ce75884eeed0f8d10d
Tim Kao <pakuya.kao@gmail.com>
2018-12-06 15:53:17 -0500

    add suffix url patterns

--- snippets/views.py
+++ snippets/views.py
@@ -8,7 +8,7 @@ from .models import Snippet
 from .serializers import SnippetSerializer
 
 @api_view(['GET', 'POST'])
-def snippet_list(request):
+def snippet_list(request, format=None):
   if request.method == 'GET':
     snippets = Snippet.objects.all()
     serializer = SnippetSerializer(snippets, many=True)
@@ -21,7 +21,7 @@ def snippet_list(request):
     return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET', 'PUT', 'DELETE'])
-def snippet_detail(request, pk):
+def snippet_detail(request, pk, format=None):
   try:
     snippet = Snippet.objects.get(pk=pk)
   except Snippet.DoesNotExist:
