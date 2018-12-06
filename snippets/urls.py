a1cc44a2bf5116d10feb6a5bbbb16c0c008af298
Tim Kao <pakuya.kao@gmail.com>
2018-12-06 18:31:39 -0500

    linked two models and serializers

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import SnippetList, SnippetDetail
+from .views import SnippetList, SnippetDetail, UserList, UserDetail
 
 urlpatterns = [
   path('snippets/', SnippetList.as_view()),
   path('snippets/<int:pk>', SnippetDetail.as_view()),
+  path('users/', UserList.as_view()),
+  path('users/<int:pk>', UserDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- snippets/views.py
+++ snippets/views.py
@@ -2,13 +2,27 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework import generics
+from django.contrib.auth.models import User
 from .models import Snippet
-from .serializers import SnippetSerializer
+from .serializers import SnippetSerializer, UserSerializer
+
 
 class SnippetList(generics.ListCreateAPIView):
   queryset = Snippet.objects.all()
   serializer_class = SnippetSerializer
 
+  def perform_create(self, serializer):
+    serializer.save(owner=self.request.user)
+
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
   queryset = Snippet.objects.all()
   serializer_class = SnippetSerializer
+
+class UserList(generics.ListAPIView):
+  queryset = User.objects.all()
+  serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+  queryset = User.objects.all()
+  serializer_class = UserSerializer
+
