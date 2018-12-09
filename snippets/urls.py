92257489002292319f767ed08011aa67038d23f8
Tim Kao <pakuya.kao@gmail.com>
2018-12-09 12:56:39 -0500

    linked two models with HyperlinkedModelSerializer

--- snippets/urls.py
+++ snippets/urls.py
@@ -4,11 +4,11 @@ from .views import SnippetList, SnippetDetail, UserList, UserDetail, api_root, S
 
 urlpatterns = [
   path('snippets/', SnippetList.as_view(), name='snippet-list'),
-  path('snippets/<int:pk>', SnippetDetail.as_view()),
+  path('snippets/<int:pk>', SnippetDetail.as_view(), name='snippet-detail'),
   path('users/', UserList.as_view(), name='user-list'),
-  path('users/<int:pk>', UserDetail.as_view()),
+  path('users/<int:pk>', UserDetail.as_view(), name='user-detail'),
   path('', api_root),
-  path('snippets/<int:pk>/highlight/', SnippetHighlight.as_view())
+  path('snippets/<int:pk>/highlight/', SnippetHighlight.as_view(), name='snippet-highlight')
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
